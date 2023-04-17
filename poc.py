5b95e0d1543d2a8209febac69085195925e46f59
Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>
2023-04-17 19:50:00 +0530

    Update poc.py

--- poc.py
+++ poc.py
@@ -1,3 +1,13 @@
 import os
 
 x = [i for i in range(10)]
+
+def foo(x, y):
+    if x > y:
+        print(x)
+    
+    if x == y or x + 1 > y:
+        print(y)
+    
+    if x + y == 42:
+        return 42
